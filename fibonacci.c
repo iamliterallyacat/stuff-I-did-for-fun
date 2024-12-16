dc3245e587716a5c6d6b3838f54900dd2e87424b
Hannah <161227095+iamliterallyacat@users.noreply.github.com>
2024-12-16 13:03:53 +0100

    added argv functionality to fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main () {
-int fibCount = 0;
+int main (int argc, char* argv[]) 
+{
+int fibLimit = atoi(argv[1]);
 int fibT = 0;
 int fib2 = 1;
 int fib1 = 0;
 
-	while (fibCount < 15 ) {
-	
+	while (1 < fibLimit ) 
+	{
 	fib1 = fib2 + fib1; //add r8, r9 
 	fibT = fib2;	    
 	fib2 = fib1;        
         fib1 = fibT;
-	fibCount ++;	
+	fibLimit --;	
 	}
 
-printf("%d", fib2);
+printf("%d\n", fib2);
 return 0;
 }
